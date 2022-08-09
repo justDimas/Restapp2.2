4c311a99e7fa1083589a9031ec92833450290361
justDimas <dmitriy.smal@mail.ru>
2022-08-09 16:49:31 +0300

    Write upload data script in js and good.html for pizzas and salads

--- src/main/resources/static/js/good.js
+++ src/main/resources/static/js/good.js
@@ -1,17 +1,21 @@
 function ready() {
-    let fileInputs = [document.getElementById("drink-image-add-input"),
+    let fileInputs = [document.getElementById("pizza-image-add-input"),
+                      document.getElementById("pizza-image-update-input"),
+                      document.getElementById("salad-image-add-input"),
+                      document.getElementById("salad-image-update-input"),
+                      document.getElementById("drink-image-add-input"),
                       document.getElementById("drink-image-update-input")];
     fileInputs.forEach(item => item.addEventListener('change',()=>{
         let img = document.getElementById(event.target.dataset.toggleId);
         img.src = (!event.target.files.length) ? "images/noimg.jpg" : "images/" + event.target.files[0].name;
     }));
 
-    let forms = [document.getElementById("add-pizza-form"),
-                 document.getElementById("update-pizza-form"),
-                 document.getElementById("add-salad-form"),
-                 document.getElementById("update-salad-form"),
-                 document.getElementById("add-drink-form"),
-                 document.getElementById("update-drink-form")];
+    let forms = [document.getElementById("pizza-add-form"),
+                 document.getElementById("pizza-update-form"),
+                 document.getElementById("salad-add-form"),
+                 document.getElementById("salad-update-form"),
+                 document.getElementById("drink-add-form"),
+                 document.getElementById("drink-update-form")];
     forms.forEach(item => item.classList.add("hidden"));
 }
 document.addEventListener("DOMContentLoaded", ready);
@@ -36,16 +40,74 @@ let t = new class{
 
 document.onclick = function(event){
     switch (event.target.id) {
-        case "show-add-pizza-button":
-        case "show-add-salad-button":
-        case "show-add-drink-button": {
+        case "pizza-add-toggle":
+        case "salad-add-toggle":
+        case "drink-add-toggle": {
             let form = document.getElementById(event.target.dataset.toggleId);
             t.toggle(form);
             break;
         }
-        case "show-update-pizza-button": {break;}
-        case "show-update-salad-button": {break;}
-        case "show-update-drink-button": {
+        case "pizza-update-toggle": {
+            let form = document.getElementById(event.target.dataset.toggleId);
+            let name = document.getElementById("pizza-name-update-input");
+            let price =  document.getElementById("pizza-price-update-input");
+            let vegetarian = document.getElementById("pizza-vegetarian-update-input");
+            let spicy = document.getElementById("pizza-spicy-update-input");
+            let image = document.getElementById("pizza-image-update-form");
+            let pizza;
+            for(item of pizzas){
+                if(item.pizzaId == event.target.getAttribute("value")){
+                    pizza = item;
+                    break;
+                }
+            }
+            if(pizza === undefined) break;
+            if(t.toggle(form)){
+                name.value = pizza.good.goodName;
+                price.value = pizza.good.goodPrice;
+                vegetarian.checked = pizza.vegetarian;
+                spicy.checked = pizza.spicy;
+                image.src = "images/" + pizza.pizzaImg;
+            }else{
+                name.value = "";
+                price.value = "";
+                vegetarian.checked = false;
+                spicy.checked = false;
+                image.src = "";
+            }
+            break;
+        }
+        case "salad-update-toggle": {
+            let form = document.getElementById(event.target.dataset.toggleId);
+            let name = document.getElementById("salad-name-update-input");
+            let price =  document.getElementById("salad-price-update-input");
+            let vegetarian = document.getElementById("salad-vegetarian-update-input");
+            let warm = document.getElementById("salad-warm-update-input");
+            let image = document.getElementById("salad-image-update-form");
+            let salad;
+            for(item of salads){
+                if(item.saladId == event.target.getAttribute("value")){
+                    salad = item;
+                    break;
+                }
+            }
+            if(salad === undefined) break;
+            if(t.toggle(form)){
+                name.value = salad.good.goodName;
+                price.value = salad.good.goodPrice;
+                vegetarian.checked = salad.vegetarian;
+                warm.checked = salad.warm;
+                image.src = "images/" + salad.saladImg;
+            }else{
+                name.value = "";
+                price.value = "";
+                vegetarian.checked = false;
+                warm.checked = false;
+                image.src = "";
+            }
+            break;
+        }
+        case "drink-update-toggle": {
             let form = document.getElementById(event.target.dataset.toggleId);
             let name = document.getElementById("drink-name-update-input");
             let price =  document.getElementById("drink-price-update-input");
@@ -61,6 +123,7 @@ document.onclick = function(event){
                     break;
                 }
             }
+            if(drink === undefined) break;
             if(t.toggle(form)){
                 name.value = drink.good.goodName;
                 price.value = drink.good.goodPrice;
