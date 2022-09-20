984761931b496738bfae07d06105da859ee89049
justDimas <dmitriy.smal@mail.ru>
2022-09-20 18:32:33 +0300

    Add nav panel, write wrong password logic script

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,7 @@
 let modal;
 let modalToggle;
+let urlParams;
+let errorBlock;
 
 function toggle(){
     modal = document.querySelector(".modal");
@@ -16,6 +18,15 @@ function ready() {
     modalToggle = document.querySelectorAll(".toggle");
     modal = document.querySelector(".modal");
     modalToggle.forEach(item => item.addEventListener("click", toggle));
-    modal.classList.add("hidden");
+
+    urlParams = new URLSearchParams(window.location.search);
+    errorBlock = document.querySelector(".modal-window *:last-child");
+    if(urlParams.has("error")){
+        document.querySelector(".modal-window *:last-child");
+        errorBlock.style.color = "red";
+    }else{
+        modal.classList.add("hidden");
+        errorBlock.style.display = "none";
+    }
 }
 document.addEventListener("DOMContentLoaded", ready);
